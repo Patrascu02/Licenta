089c2cdd35162397aaf605a51a2408c761516e1a
Patrascu02 <alexandru.patrascu3@s.unibuc.ro>
2025-12-28 19:21:29 +0200

    2

--- Models/Scouting/ScoutPlayer.cs
+++ Models/Scouting/ScoutPlayer.cs
@@ -7,6 +7,7 @@ namespace Licenta.Models.Scouting
         public int ScoutPlayerId { get; set; }
         public int ScoutId { get; set; }
         public string PlayerName { get; set; }
+        public string Observations { get; set; } // "arunca prost", "slab psihic" etc.
 
         public Scout Scout { get; set; }
     }
