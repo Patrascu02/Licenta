c4d6e9565babe63c478e66efe7d92b80a65e6915
Patrascu02 <alexandru.patrascu3@s.unibuc.ro>
2026-01-07 23:24:37 +0200

    ss

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO Staff (UserId, FirstName, LastName, DateOfBirth, HireDate, ExperienceYears)
+VALUES ('e9ee290e-fc00-400c-98f0-58a13815c860', 'Patrascu', 'Alexandru', '1990-01-01', GETDATE(), 10);
